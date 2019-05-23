4fce1bccb22a48812165518ed6047415fd32b19c
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-05-23 13:49:43 +0900

    edit_carriawave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,11 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
+  set :default_env, {
+    rbenv_root: "/usr/local/rbenv",
+    path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+    IMG_UP_AWS_S3_ACCESS_KEY_ID: ENV["IMG_UP_AWS_S3_ACCESS_KEY_ID"],
+    IMG_UP_AWS_S3_SECRET_ACCESS_KEY: ENV["IMG_UP_AWS_S3_SECRET_ACCESS_KEY"]
   }
 
   config.fog_directory  = 'chat-space-upload-0000'
