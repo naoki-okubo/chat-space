4eaa1f1786746b1387e10ff1cde1ce985e3fe0ed
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-05-23 14:05:43 +0900

    d

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,8 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-# Default value for default_env is {}
+
 
 
   config.fog_directory  = 'chat-space-upload-0000'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-upload-0000'
-end
