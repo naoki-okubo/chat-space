642d49c1a34d27dcfc563b8a816e3bf6c53e8581
Naoki Okubo <naoki.okubo.1108@icloud.com>
2019-05-23 14:04:02 +0900

    d

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 # Default value for default_env is {}
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  IMG_UP_AWS_S3_ACCESS_KEY_ID: ENV["IMG_UP_AWS_S3_ACCESS_KEY_ID"],
-  IMG_UP_AWS_S3_SECRET_ACCESS_KEY: ENV["IMG_UP_AWS_S3_SECRET_ACCESS_KEY"]
-}
+
 
   config.fog_directory  = 'chat-space-upload-0000'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-upload-0000'
